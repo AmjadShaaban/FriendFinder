03be65548e7819e4a4e8890b5ff835bced015fe1
Amjad Shaaban <theamjad@outlook.com>
2019-11-12 09:54:09 -0500

    Added start script in package.json

--- app/routes/apiRoutes.js
+++ app/routes/apiRoutes.js
@@ -17,21 +17,11 @@ module.exports = function (app) {
       }
       // using array reduce function to add total Difference
       totalDifference[i] = scores.reduce((a, b) => a + b, 0)
-      console.log("scores difference for friends array at index: " + i + "\n")
-      console.log(scores)
-      scores = [];
-
     }
-    console.log("wiping scores array after each friends element\\n")
-    console.log(scores)
-    console.log("\n\ntotal Difference between each friends element\nSO, the index of the lowest value in the array if the closest frined match.\n")
-    console.log(totalDifference)
     //Finding the index of the lowest value
     var min = Math.min.apply(null, totalDifference);
     //This is ALSO the index of the matching friend from friends array, I know I LOVE MATH
     var match = totalDifference.indexOf(min);
-    console.log(match);
-
     friends.push(req.body);
     //send the matching friend JSON data as response to the AJAX call
     res.json(friends[match]);
